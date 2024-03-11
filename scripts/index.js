7fd38e028fad2dba8286da03d3f0aed93b8510df
GPGS <sfontato1993@gmail.com>
2024-03-10 21:29:31 -0500

    actualizacion dia 10 03 24

--- scripts/index.js
+++ scripts/index.js
@@ -9,34 +9,43 @@ class Activity{
 
 class Repository {
     constructor(){
-        this.actividades=[];
-        this.nextId = 1; // ID inicial
+        this.arrayactividades=[];
+       
     }
 
     agregarDatos(dato) {
-        Activity.id = this.nextId++; // Asignar el próximo ID único
-        this.actividades.push(dato);
+      
+        this.arrayactividades.push(dato);
     }
 
     obtenerDatos() {
-        return this.actividades;
+        return this.arrayactividades;
     }
 
-    eliminarActividad(id) {
-        this.actividades = this.actividades.filter(activity => activity.id !== id);
+    borrarcarta(id) {
+        this.arrayactividades = this.arrayactividades.filter(activity => activity.id !== id);
+      
     }
 
 }
 
+
+let contador = 0; // Variable global para almacenar el número
+
 const repo = new Repository();
 
 function guardarValores() {
-    
+    contador++;
     const actividad = document.getElementById('actfavorita').value;
     const desd = document.getElementById('desd').value;
     const linkimg = document.getElementById('linkimg').value;
 
-    const datos = new Activity(actividad, desd, linkimg);
+    // Verificar que los campos obligatorios no estén vacíos
+    if (actividad.trim() === '' || desd.trim() === '' || linkimg.trim() === '') {
+        alert('Por favor, complete todos los campos obligatorios.');
+        return;
+    }
+    const datos = new Activity(contador,actividad, desd, linkimg);
     repo.agregarDatos(datos);
 
     console.log(repo.obtenerDatos());
@@ -55,16 +64,18 @@ function mostrarActividades() {
     resultadoDiv.innerHTML = '';
 
     // Iteración sobre todas las actividades almacenadas en el repository
-    repo.obtenerDatos().forEach(actividades => {
+    repo.obtenerDatos().forEach(arrayactividades => {
         // Creación de un nuevo div para cada actividad
         const actividadDiv = document.createElement('div');
         actividadDiv.classList.add('tarjeta');
+        
         // Asignación del contenido HTML del div con los datos de la actividad
         actividadDiv.innerHTML = `
-            <h1>${actividades.title}</h1><br>
-            <p>${actividades.description}</p><br>
-            <img src="${actividades.imgUrl}" alt="${actividades.title}">
-            <button class="button" data-id="${actividades.id}" onclick="eliminarActividad(${actividades.id})">Eliminar</button>
+        
+            <h3>${arrayactividades.title}</h3><br>
+            <p>${arrayactividades.description}</p><br>
+            <img src="${arrayactividades.imgUrl}" alt="${arrayactividades.title}">
+            <button class="btn" data-id="${arrayactividades.id}" onclick="eliminarActividad(${arrayactividades.id})">Eliminar</button>
         `;
         
         // Agregado del div de la actividad al div 'resultado'
@@ -73,6 +84,6 @@ function mostrarActividades() {
 }
 
 function eliminarActividad(id) {
-    repo.eliminarActividad(id);
+    repo.borrarcarta(id);
     mostrarActividades();
 }
